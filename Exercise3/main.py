5f42e6e7e5fe76ea39d3ed2c00b9a3262571d3e2
zuri11 <zukaa.aleksaia@gmail.com>
2021-12-17 19:17:26 +0400

    fixed a small bug

--- Exercise3/main.py
+++ Exercise3/main.py
@@ -1,36 +1,38 @@
-r = 0
-c = 0
-n = 0
-
-def bomberMan(n, grid):
-    if n == 1:
-        return grid
-
-    if n % 2 == 0:
-        return ['O'*c for i in range(r)]
-
-    n //=2 
-
-    for j in range((n+1) % 2 + 1):
-        new_grid = [['O']*c for i in range(r)]
-        def set(x, y):
-            if 0 <= x < r and 0 <= y < c:
-                new_grid[x][y] = '.'
-
-        xi = [0, 0, 0, 1, -1]
-        yi = [0, -1, 1, 0, 0]
-
-        for x in range(r):
-            for y in range(c):
-                if grid[x][y] == 'O':
-                    for i, q in zip(xi, yi):
-                        set(x+i, y+q)
-
-        grid = new_grid
-
-    return ["".join(x) for x in grid]
-
 def main():
+    def bomberMan(n, grid):
+        print(r)
+        if n == 1:
+            return grid
+
+        if n % 2 == 0:
+            return ['O'*c for i in range(r)]
+
+        n //= 2 
+
+        for j in range((n+1) % 2 + 1):
+            print(r)
+            print(c)
+            print(n)
+            new_grid = [['O']*c for i in range(r)]
+            print(new_grid)
+            def set(x, y):
+                if 0 <= x < r and 0 <= y < c:
+                    new_grid[x][y] = '.'
+
+            xi = [0, 0, 0, 1, -1]
+            yi = [0, -1, 1, 0, 0]
+
+            for x in range(r):
+                for y in range(c):
+                    if grid[x][y] == 'O':
+                        for i, q in zip(xi, yi):
+                            set(x+i, y+q)
+
+            grid = new_grid
+            print(new_grid)
+
+        return ["".join(x) for x in grid]
+
     output = open("output", 'w')
     f = open("input", 'r')
     lines = f.readlines()
@@ -40,6 +42,9 @@ def main():
     r = int(firstLine[0])
     c = int(firstLine[1])
     n = int(firstLine[2])
+    print(r)
+    print(c)
+    print(n)
 
     grid = []
 
@@ -47,6 +52,7 @@ def main():
         grid_item = grid_items[i]
         grid.append(grid_item)
 
+    print(bomberMan(n, grid))
     result = bomberMan(n, grid)
 
     output.write('\n'.join(result))
